ad9bb3ee15969bd11c91152a0d3fd2c5d0e986c9
natalyakirs <nik23@nau.edu>
2023-06-22 00:04:37 +0200

    hw3

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,33 @@
+package HW3;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
+
         // create an object of Queue class
+        MyQueue queue1 = new MyQueue();
 
-        // try to delete element from the queue
-
-        // currently queue is empty
-
-        // so deletion is not possible
+        // try to delete element from the queue:currently queue is empty, so deletion is not possible
+        queue1.dequeue();
 
         // insert elements to the queue
+        queue1.enqueue(1);
+        queue1.enqueue(2);
+        queue1.enqueue(3);
 
         // check when queue is full
+        queue1.enqueue(4);
 
         // removes element until it is empty
+        queue1.dequeue();
+        queue1.dequeue();
+        queue1.dequeue();
 
         // check if queue is empty
+        queue1.dequeue();
+
     }
+
 }
--- src/MyQueue.java
+++ src/MyQueue.java
@@ -1,33 +1,63 @@
-public class MyQueue {
-    private final int SIZE;
-    private int[] queue;
-    private int front, rear;
+package HW3;
 
-    public MyQueue(int SIZE) {
-        this.SIZE = SIZE;
-        queue = new int[SIZE];
-        //front = ...
-        //rear = ...
-    }
+public class MyQueue {
+    private final int SIZE = 3;
+    private int[] queue = new int[SIZE];
+    private int front = -1;
+    private int rear = -1;
+
 
     // check if the queue is full
     public boolean isFull() {
+        if(rear == SIZE - 1){
+            return true;
+        }
         return false;
     }
 
     // check if the queue is empty
     public boolean isEmpty() {
+        if(front == -1){
+            return true;
+        }
         return false;
     }
 
     // insert elements to the queue
     public void enqueue(int element) {
-
+        // first to check if the queue is full already
+        if(isFull()) {
+            System.out.println("Queue is already full, you can't insert an element " + element);
+        } else {
+            front = 0;
+            rear++;
+            queue[rear] = element;
+            System.out.println("The element " + element + " was inserted into queue.");
+            System.out.println("The queue consist of : " + queue.length + " elements");
+        }
     }
 
     // delete element from the queue
     public int dequeue() {
-        return 0;
+        int element ;
+        // first to check if the queue is empty
+        if (isEmpty()) {
+            System.out.println("Queue is empty, you can't delete an element.");
+            return (-1);
+        } else {
+            //delete first element in queue
+            element = queue[front];
+            // If queue consists of only one element - return original values to front and rear
+            if(rear == front) {
+                front = -1;
+                rear = -1;
+            } else {
+                // Last case with queue consisting of more than 1 element
+                front++;
+            }
+            System.out.println("The element " + element + " was deleted from the queue");
+            return element;
+        }
     }
 
     public int getFront() {
@@ -44,5 +74,3 @@ public class MyQueue {
     }
 
 }
-
-
